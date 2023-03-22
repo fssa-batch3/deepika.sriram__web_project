6d02520128c38a8aa277e6b4452b07190dcdc460
deepikasrm5 <deepika.sriram@fwsa.freshworks.com>
2023-03-22 13:32:35 +0530

    feat!: finished update part of doctor's details

--- CRUD forms/javascript files/form for doctor.js
+++ CRUD forms/javascript files/form for doctor.js
@@ -9,43 +9,48 @@ function uploadDoctor(e) {
     experience_type = document.getElementById("experience_type").value;
     experience_duration = document.getElementById("experience_duration").value;
     // time_availability = document.getElementById("time_availability").value;
-    Working_days = [];
-    working_days = document.querySelectorAll("input[type='checkbox']:checked");
-    for (i = 0; i < working_days.length; i++) {
-        Working_days.push(working_days[i].value)
-    }
+    Working_days = {};
+    // working_days = document.querySelectorAll("input[type='checkbox']:checked");
+    // for (i = 0; i < working_days.length; i++) {
+    //     Working_days.push({"day":working_days[i].value,"status":true})
+    // }
     // Working_days = JSON.stringify(workingDaysArray);
     Experience = {experience_duration,experience_type };
     // Experience = JSON.stringify(experienceObj);
 
+    monday = document.getElementById("Monday").checked;
+    tuesday = document.getElementById("Tuesday").checked;
+    wednesday = document.getElementById("Wednesday").checked;
+    thursday = document.getElementById("Thursday").checked;
+    friday = document.getElementById("Friday").checked;
+    saturday = document.getElementById("Saturday").checked;
+    sunday = document.getElementById("Sunday").checked;
+    // console.log(monday)
+    function condition(day, name){
+        if(day == true){
+
+           return Working_days[name] = true
+        }
+        if(day != true){
+            return Working_days[name] = false
+        }
+    }
+
+    condition(monday, "monday")
+    condition(tuesday, "tuesday")
+    condition(wednesday, "wednesday")
+    condition(thursday, "thursday")
+    condition(friday, "friday")
+    condition(saturday, "saturday")
+    condition(sunday, "sunday")
+
+    
+    
+    console.log(Working_days)
+
     starting_time = document.getElementById("from").value;
     ending_time = document.getElementById("to").value;
     button = document.querySelector("button#time");
-    Availability = {starting_time,ending_time};
-
-    // timeAvailabilityArray = [];
-
-    // button.addEventListener('click', function addTime(){
-    //     for (i = 0; i < timeAvailabilityArray.length; i++) {
-    //         if (timeAvailabilityArray.length = 0) {
-    //             timeAvailabilityArray.push({ starting_time, ending_time });
-    //             document.getElementById("from").reset();
-    //             document.getElementById("to").reset();
-    //         }
-    //         else if (timeAvailabilityArray[i]["starting_time"] != starting_time.value || timeAvailabilityArray[i]["ending_time"] != ending_time.value) {
-    //             timeAvailabilityArray.push({ starting_time, ending_time });
-    //             document.getElementById("from").reset();
-    //             document.getElementById("to").reset();
-
-    //         }
-    //         else {
-    //             alert("timing already exists!!");
-    //             document.getElementById("from").reset();
-    //             document.getElementById("to").reset();
-    //         }
-    //     }
-    // });
-    // Availability = JSON.stringify(timeAvailabilityObj);
     uuid = uuidv4();
 
 
@@ -64,16 +69,18 @@ function uploadDoctor(e) {
 
     // if condition fail
     if (!exist) {
+        Availabilities =[];
+        Availabilities.push({starting_time,ending_time});
 
         // Experience = JSON.stringify(experienceObj);
-        doctor_detail.push({ Doctor_name, Doctor_image, Qualifications, Experience, Department, Hospital_name, Working_days, Availability, uuid });
+        doctor_detail.push({ Doctor_name, Doctor_image, Qualifications, Experience, Department, Hospital_name, Working_days, Availabilities, uuid });
 
-        localStorage.setItem("unique_id", uuid);
+        localStorage.setItem("unique_id", Doctor_name);
         localStorage.setItem('doctor_detail', JSON.stringify(doctor_detail));
 
         document.querySelector('form').reset();
         alert('Detail submitted Successfully✅');
-
+         location.href = "./edit form for doctor.html"
 
     }
     // if condition pass
